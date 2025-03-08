c7d5e238aa64a488efe3e8a3d6860cd25d99ab18
Andra <smarandache.andra.e3s@student.ucv.ro>
2025-03-08 12:32:11 +0200

    Footer Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,9 @@ import { useLocation } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
-  const location = useLocation();
-  const isHomePage = location.pathname === '/';
 
   return (
-    <footer style={{ backgroundColor: isHomePage ? 'transparent' : 'white', color: isHomePage ? 'white' : 'black' }}>
+    <footer>
       <p>&copy; 2025 BatterLife. All rights reserved.</p>
     </footer>
   );
